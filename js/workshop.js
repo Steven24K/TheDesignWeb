98a4f45a8a9beb676ac4c5bd8b1277aa27979b27
Steven <s.koerts2@gmail.com>
2024-11-06 16:08:21 +0000

    More exercises

--- js/workshop.js
+++ js/workshop.js
@@ -46,7 +46,7 @@ const ListNode = (value, tail) => ({ kind: 'node', value: value, tail: tail });
 const Empty = () => ({ kind: 'empty' });
 let list1 = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5, Empty())))));
 // 1. Build a .map function for the linked list
-// 2. Use it to increment each item of the list
+// 2. Use the map function it to increment each item of the list
 // 3. Make a transform function using map to create a list of booleans. True for even values and false for odd values. 
 //    i.e. list1.map(isEven) should give the following result [false, true, false, true, false] where the array is the linked list
 // 4. Make a method list1.reduce<b>(f: (acc: b, x: a) => b, init: b): b where f is a function with 2 parameters 
@@ -56,3 +56,6 @@ let list1 = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5, Empty())
 // 5. Make a .filter() method that takes a predicate function as input. Every element is checked against the predicate and 
 //    and if the element matches the condition it is added to a new list. 
 //    Hint: Use the reduce() function to implement the filter method.
+// 6. Make a .toString() method using the .reduce() function. The function transforms the list insto a single formatted string 
+//    Where every value is divided by a comma.
+// 7. Go back to the .map function from question 1 and use reduce to implement map().
