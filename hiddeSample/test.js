b0f993ca4d1cbfc66b054f34dcc55a7a910be006
HJS-18 <138207015+HJS-18@users.noreply.github.com>
2024-11-13 14:41:46 +0100

    Recursion

--- hiddeSample/test.js
+++ hiddeSample/test.js
@@ -1,1 +1,48 @@
-console.log("Hello");
+var Some = function (v) { return ({ kind: 'left', value: v }); };
+/*
+const Some = <a>(v: a): Option<a> => {
+    return {kind: 'left', value: v}
+}
+*/
+var None = function () { return ({ kind: 'right', value: false }); };
+/*
+const None = <a>(): Option<a> => {
+    return {kind: 'right', value: false}
+}
+*/
+var x = None();
+var y = null;
+y + 1;
+if (x.kind == 'left') {
+    x.value + 1;
+}
+// 2.
+// Iterative function
+var sumUpTo = function (n) {
+    var sum = 0;
+    for (var i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+};
+// Recursive function
+var sumUpToRec = function (n) {
+    if (n == 0) {
+        return 0;
+    }
+    return n + sumUpToRec(n - 1);
+};
+// Checks if the two functions are equal for all n from 0 to n
+var checkRecWithRec = function (n) {
+    if (sumUpTo(n) == sumUpToRec(n)) {
+        return true;
+    }
+    else if (n == 0) {
+        return false;
+    }
+    else {
+        return checkRecWithRec(n - 1);
+    }
+};
+// Checks if the two functions are equal for all n from 0 to n
+console.log(sumUpTo(5) == sumUpToRec(5));
--- hiddeSample/test.ts
+++ hiddeSample/test.ts
@@ -1,1 +1,61 @@
-console.log("Hello")
+// 1.
+type Sum<a,b> = { kind: 'left', value: a } | { kind: 'right', value: b }
+
+type Option<a> = Sum<a, false>
+const Some = <a>(v: a): Option<a> => ({kind: 'left', value: v})
+
+/*
+const Some = <a>(v: a): Option<a> => {
+    return {kind: 'left', value: v}
+}
+*/
+
+const None = <a>(): Option<a> => ({kind: 'right', value: false})
+
+/*
+const None = <a>(): Option<a> => {
+    return {kind: 'right', value: false}
+}
+*/
+
+let x: Option<number> = None()
+let y: number = null!
+
+y + 1
+
+if (x.kind == 'left') {
+    x.value + 1
+}
+
+
+// 2.
+// Iterative function
+const sumUpTo = (n: number): number => {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+};
+
+// Recursive function
+const sumUpToRec = (n: number): number => {
+    if (n == 0) {
+        return 0
+    }
+    return n + sumUpToRec(n-1)
+};
+
+// Checks if the two functions are equal for all n from 0 to n
+const checkRecWithRec = (n: number): boolean => {
+    if (sumUpTo(n) == sumUpToRec(n)) {
+        return true
+    } else if (n == 0) {
+        return false
+    } else {
+        return checkRecWithRec(n-1)
+    }
+};
+
+// Checks if the two functions are equal for all n from 0 to n
+console.log(sumUpTo(5) == sumUpToRec(5))
