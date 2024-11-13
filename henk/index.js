b4d336b5e53412d5a2a4e2be8cbfce353e10f872
RFlintstone <rubenflint@hotmail.com>
2024-11-13 14:45:47 +0100

    Bobby dont likes math, only dropping tables

--- henk/index.js
+++ henk/index.js
@@ -1,3 +1,12 @@
 "use strict";
 const henk = { henk: "Henk" };
-console.log(typeof henk);
+console.log(typeof henk); // object, no Henk :(
+// ================================
+const sumUpTo = (n) => {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+};
+console.log(sumUpTo(5));
