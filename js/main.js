955a8f35501096a0fd5ec36f8aa269950a20a026
Steven <s.koerts2@gmail.com>
2024-11-13 08:57:56 +0000

    Update site list

--- js/main.js
+++ js/main.js
@@ -23,6 +23,12 @@ const pages = [
         description: "ぐずぐず煮え切らない。甘い迷い。僕は優柔不断な男だろうか、それすらも決められない。",
         link: { text: "look away!", url: "./優柔不断/index.html" },
         image: { alt: "Cool pic", url: "./the-basic-styleguide/images/wallpaper2.jpg" }
+    },
+    {
+        title: "The Parkinson Styleguide",
+        description: "This guide helps you get started with parkinson",
+        link: { text: "Check this out!", url: "./the-parkinson-styleguide/index.html" },
+        image: { alt: "Design Thumbnail", url: "./the-parkinson-styleguide/images/old-man-symptoms-parkinson-disease_24911-63254.avif" }
     }
 ];
 const webpages_list = document.querySelector("#webpages-list");
